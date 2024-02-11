6b687bede893d541fc36e3efe35a5c1e06a33d0e
rocmalone <114255759+rocmalone@users.noreply.github.com>
2024-02-10 22:38:09 -0800

    Print client IP on connect

--- src/main.c
+++ src/main.c
@@ -1,12 +1,24 @@
+#include <arpa/inet.h>
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <unistd.h>
+#include <string.h>
+
 
 #define PORT 8080
+#define BUFFER_SIZE 1024 /* Used for reading files from socket */
 
 int main() {
+    char buffer[BUFFER_SIZE];
+    char resp[] = "HTTP/1.0 200 OK\r\n"
+                "Server: webserver-c\r\n"
+                "Content-type: text/html\r\n\r\n"
+                "<html>hello, world</html>\r\n";
+
+
     // Create a socket
     //   AF_INET: Socket domain for IPv4
     //   SOCK_STREAM: reliable byte streams for TCP
@@ -20,6 +32,8 @@ int main() {
     }
     printf("Socket created successfully\n");
 
+
+    // Create a local socket addr with which to bind the socket
     struct sockaddr_in host_addr;
     int host_addrlen = sizeof(host_addr);
 
@@ -29,11 +43,17 @@ int main() {
        Recall: 1 in big endian is 0 0 0 1 vs. 1 0 0 0 in little endian */
     host_addr.sin_port = htons(PORT);
 
-    /* INADDR_ANY accepts messages from any network adapter.
+    /* Create an internet address for the socket
+       INADDR_ANY accepts messages from any network adapter.
        It translates to 0.0.0.0 */
     struct in_addr host_sin_addr;
     host_sin_addr.s_addr = htonl(INADDR_ANY);
-    host_addr.sin_addr.s_addr; 
+    host_addr.sin_addr = host_sin_addr;
+
+    // Create client address
+    struct sockaddr_in client_addr;
+    int client_addrlen = sizeof(client_addr);
+
 
     /* Bind socket to the address 
        (struct sockaddr *)&host_addr casts the mem addr of host_addr (type 
@@ -64,9 +84,37 @@ int main() {
         }
 
         printf("Connection accepted\n");
+
+        // Get client address (return: client socket addr size)
+        int sockn = getpeername(newsockfd, (struct sockaddr *)&client_addr,
+                                (socklen_t *)&client_addrlen);
+        if (sockn < 0) {
+            perror("[ERR] Webserver (getsockname)");
+            continue;
+        }
+
+
+        // Read from the socket
+        int valread = read(newsockfd, buffer, BUFFER_SIZE);
+        if (valread < 0) {
+            perror("[ERR] Webserver (Read)");
+            continue;
+        }
+
+        // Print client IP
+        printf("[%s:%u]\n", inet_ntoa(client_addr.sin_addr), 
+                ntohs(client_addr.sin_port));
+
+        // Write to the socket
+        int valwrite = write(newsockfd, resp, strlen(resp));
+        if (valwrite < 0) {
+            perror("[ERR] Webserver (Write)");
+            continue;
+        }
+
+        // Make fd no longer refer to any file, which closes the socket
         close(newsockfd);
     }
-
 
     return 0;
 }
